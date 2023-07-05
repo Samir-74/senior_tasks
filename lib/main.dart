2dd00c91ae5a006e9b1011a2e316c85fdc642870
Samir <sm4119641@gmail.com>
2023-07-05 17:06:48 +0300

    Add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'task-3/register-screen.dart';
+import 'task-4/messages-screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: RegisterScreen(),
+      home: MessagesScreen(),
     );
   }
 }
